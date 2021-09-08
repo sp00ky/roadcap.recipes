a6ba52bf4991a5c318d2cc78464df97291ac372b
Kelly Harrison <kelly@sbsoftwareshop.com>
2021-09-08 15:44:17 -0400

    Added veggie theme using css from github.com/hayanisaid/bootstrap4-website

--- roadcap.recipies/wwwroot/js/site.js
+++ roadcap.recipies/wwwroot/js/site.js
@@ -1,4 +1,11 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function () {
+    $('.header').height($(window).height());
 
-// Write your JavaScript code.
+    $(".navbar a").click(function () {
+        $("body,html").animate({
+            scrollTop: $("#" + $(this).data('value')).offset().top
+        }, 1000)
+
+    })
+
+})
